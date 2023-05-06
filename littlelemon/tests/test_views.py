5203b4510f9f758de282fba7ec2558d44cd782f7
Aissam QARCHI <issam.imm@gmail.com>
2023-05-06 11:22:50 +0000

    Adding README

--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -1,11 +1,26 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from restaurant.serializers import MenuSerializer
+from rest_framework.test import APIClient
 from restaurant.models import Menu
 from rest_framework import status
 
 
-class MenuViewTest(TestCase):
+class MenuItemViewTest(TestCase):
     def setUp(self):
-        self.pizza = Menu.objects.create(title='Pizza', price=10, inventory=50)
-        self.burger = Menu.objects.create(title='Burger', price=7, inventory=20)
-    
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='user@2023')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.MenuItem1 = Menu.objects.create(title='Pizza', price=10, inventory=50)
+        self.MenuItem2 = Menu.objects.create(title='Burger', price=7, inventory=30)
+
+    def test_getall(self):
+        url = reverse('MenuItem-view')
+        response = self.client.get(url)
+        MenuItem = MenuItem.objects.all()
+        serializer = MenuSerializer(MenuItem, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assert_(response.status_code, status.HTTP_200_OK)
